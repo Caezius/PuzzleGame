cce5aa72d699c7b290f3952bfbb7f2f13c110d45
NotRick <69263749+Caezius@users.noreply.github.com>
2022-04-09 18:34:48 -0500

    Update _functions.py
    generateObjects() function now made
    objectsToList also created, so now we can update the changes in the objects to the map array each turn

--- _functions.py
+++ _functions.py
@@ -31,7 +31,7 @@ def reset_Board_(twoDArr):
             twoDArr[row][column] = " "
 
 #waits for an arrow  key press, then returns the move name as a string
-def checkFor(playerObj):
+def checkForMove(playerObj):
     print("Press an arrow key to move")
 
     move_Pressed = False #var we make to see if the player has made a move, turns true when the player presses the right arrow
@@ -60,21 +60,40 @@ def checkFor(playerObj):
     return name_of_move
 
 
-#loops through the objects that we know are in play, and updates each of their location attributes
-def update_Objects_():
-    for position in _setupStuff.objs_In_Play.keys(): #loops through every object in play
-        obj = _setupStuff.objs_In_Play[position] #gets object at the objs_In_Play position
-        current_Obj_Location = [obj.row_num, obj.column_num] #gets the location of the object, puts in an array
+def generateObjects(level_arr, object_repr, objects_holder):
+    assigned_id = 0
+    
+    for y in range(level_arr.length):
+        
+        for x in range(level_arr[y].length):
+            
+            if (level_arr[y][x] == object_repr):
+                new_rockObj = Rock(id = assigned_id, x=x, y=y, repr = object_repr)
+                objects_holder.append(new_rockObj);
+                assigned_id = assigned_id + 1 #increase the assigned id
+                
+#takes a objects_list and the map_arr, and will first clear the entire board and then add in the objects where they are needed    
+#edits the array, doesn't return a new copy
+def objectsToList(empty_repr, obj_repr, objects_list, map_arr):
+    num_rows = map_arr.length
+    
+    #loop through every position in the map_arr and set it to the empty_repr, essentially clearing the ENTIRE board
+    for row in range(num_rows):
+        current_row = map_arr[row]
+        
+        for column in range(current_row.length):
+            map_arr[row][column] = empty_repr
+    
+    #for each object, take it's x and Y values, and then set that location in the map array to an the representation for an object
+    for each_object in objects_list:
+        
+        obj_row = each_object.y
+        obj_column = each_object.x 
+        
+        #set that position to have the representation for the object 
+        map_arr[obj_row][obj_column] = obj_repr
 
-        removed_key = _setupStuff.objs_In_Play.pop(position, "(in update_All_() function) Error: No Such Position in objs_In_Play array") #gets rid of the old position key in objs_In_Play
-        #and sets the removed object at that position to removed_key, or if this position key isnt in objs_In_Play, sets it
-        objs_In_Play[f"{current_Obj_Location}"] = obj #stores the object at the object's location
-
-def add_Objects():
-    for key in _setupStuff.objs_In_Play.keys():
-        row_num, column_num = key.split(",")
-        the_Board.map[row_num][column_num] = objs_In_Play[key]
-
+    
 #takes in a 2D array (the map) as input, and returns an array with a random row, random column
 def generate_Random_Pos_(twoD_Array):
     max_row_indx = len(twoD_Array) - 1
@@ -84,4 +103,7 @@ def generate_Random_Pos_(twoD_Array):
     rand_column = random.randint(0, max_column_indx)
 
     return [rand_row, rand_column]
+
+
+
 
