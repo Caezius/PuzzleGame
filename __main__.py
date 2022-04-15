937dfc7983c77dedcb166315807317bd0ca33683
NotRick <69263749+Caezius@users.noreply.github.com>
2022-04-14 22:58:49 -0500

    Update __main__.py
    1. Added a win object and the win condition code for reaching that object's position.
    
    1.1 Added in WinElement(GameObject) class for the win element. The only difference is that WinElement() objects will always have their id_num set to "WIN". Though currently, since validMove() is checking where the visual sprite is in the visual level array instead of the x and y attributes of the object in object_arr, creating the WinElement() class doesn't do anything. 
    
    1.2. Added win_repr variable in _setupStuff.py and added in the extra conditional to make sure that, along with the rock sprite and empty sprite, that the user can't enter the same character as the win sprite.
    
    1.3. Added in an if statement in the validMove() method (of the Map object in _setupStuff) that will return "WIN" if the win sprite is at the move position. Checks it *after* the if statements checking if the move goes off the bounds. 
    This means the statement can't accidentally get you a win if the win element is at the opposite end, and your x/y value is decreased to -1 and the object variable gets set to the sprite at the other side of the array. Actually, this shouldn't happen as manipulating the objects relies on findAndSetObj() which uses x and y coordinates. 
    If those coordinates don't exactly match the object's.x and .y, that object won't get edited (even if array-wise, those coordinates would get the exact same object)
    
    2. Made code to catch error when the user inputs a level number that isn't a key in the levels dictionary. Also put that new try-except block and the new while loop repeatedly asking for the level numbers into a function called askForLevels().
    
    3. Replaced the condition for stopping the game with a boolean called continueGame. continueGame doesn't really do anything in regards to the win condition, however, because the if statement for a win has to be checked first, and we want to print a custom message as soon as the win condition has been met. So we print the message, and then immediately ditch the while loop using "break".
    
    4. Various user experience changes
    4.1. Added code to print instructions for the user at the start of the game.
    
    4.2. Decreased pause time per turn from 2 seconds to 1 second.  (time.sleep(2) >> time.sleep(1))
    
    4.3. Added in showing the board when the game ends, along with a message.

--- __main__.py
+++ __main__.py
@@ -1,8 +1,8 @@
-from _setupStuff import levels, rock_repr, empty_repr, player_repr, Map, ObjectsHolder
+from _setupStuff import levels, rock_sprite, empty_sprite, win_sprite, Map, ObjectsHolder
 
-from _gameClasses import Player, Rock
+from _gameClasses import Player, Rock, GameObject
 
-from _functions import generateObjects, objectsToMap, askForAvatar, findPlayerPosition, getMove, showBoard
+from _functions import askForLevel, askForAvatar, generateObjects, objectsToMap, findPlayerPosition, getMove, showBoard
 
 import time
 #move = getMove()
@@ -14,59 +14,66 @@ print("Welcome to the Puzzle Game.");
 
 #get levels and create the map object and the Objects_in_play object ⬇️
 
-#get a level number from the user
-level_num = input("Please enter a level number: ");
-
-#get the level itself using the entered level_num (level_num is a string)
-selected_level = levels[level_num];
+selected_level = askForLevel(levels)
 
 #create the Puzzle board, which is a map object so we can run methods on it to search itself.
-Puzzle_board = Map(selected_level, empty_repr);
+Puzzle_board = Map(selected_level["array"], empty_sprite);
 
 #create an array of objects, then assign that array to an ObjectsHolder object so we can run methods on that array.
-array_of_objects = generateObjects(rock_repr, Puzzle_board.map, Rock);
+array_of_objects = generateObjects(repr = selected_level["rock_representation"], sprite = rock_sprite, level_arr = Puzzle_board.map, object_class = Rock);
 Objects_in_play = ObjectsHolder()
 Objects_in_play.arr = array_of_objects
 
+win_element = generateObjects(repr = selected_level["win_representation"], sprite = win_sprite, level_arr = Puzzle_board.map, object_class = GameObject)
+win_element = win_element[0]
 
+Objects_in_play.arr.append(win_element)
 #get the character's avatar ⬇️
 
 print("please enter a letter or one character (Ex: '%' or '+') to represent your player.");
 
-player_avatar = askForAvatar(empty_repr, rock_repr);
 
-player_x_pos, player_y_pos = findPlayerPosition(player_repr, selected_level);
+player_sprite = askForAvatar(empty_sprite, rock_sprite, win_sprite);
 
-player = Player(sprite = player_avatar, id_num = player_avatar, x = player_x_pos, y = player_y_pos);
+player_x_pos, player_y_pos = findPlayerPosition(selected_level["player_representation"], selected_level["array"]);
+
+player = Player(sprite = player_sprite, id_num = player_sprite, x = player_x_pos, y = player_y_pos);
 
 #add the player to the Objects_in_play array
 Objects_in_play.arr.append(player);
 
-new_map = objectsToMap(Objects_in_play.arr, Puzzle_board.map, empty_repr)
+new_map = objectsToMap(Objects_in_play.arr, Puzzle_board.map, empty_sprite)
 #set the Puzzle_board.map to the updated map
 Puzzle_board.map = new_map
 
 
-time_counter = 0;
-timeout = 6;
-#note: replace "time" and "timeout" counter with "endGame" boolean variable
-while (time_counter < timeout):
+
+moves_left = 6;
+continueGame = True;
+
+print("\nGood Day. Your mission: get that fat 'W'. \nMove into blocks to push them around. \nBut be careful. \nYou have limited moves, and wrong moves will still cost you as well.\n")
+while (continueGame == True):
 
     #print the puzzle board at the START of each turn
     showBoard(Puzzle_board.map)
-
+    print("moves left:", moves_left)
     #getMove() asks for a move from the user, ni the form of a key press
     move_name = getMove();
 
-    player_index = Objects_in_play.getIndexOfObj(obj_id = player_avatar)
+    player_index = Objects_in_play.getIndexOfObj(obj_id = player_sprite)
     player_x = Objects_in_play.arr[player_index].x;
     player_y = Objects_in_play.arr[player_index].y
 
     move_message = Puzzle_board.validMove(player_x, player_y, move_name)
-    print(move_message)
 
-    if (move_message == "yes." or move_message == "push."):
-        player_indx = Objects_in_play.getIndexOfObj(player_avatar)
+    if (move_message == "WIN"):
+        print("\nAYYY YOU GOT THAT W!!!!!!!!!!!!!!!\n")
+        Puzzle_board.map[player_y][player_x] = empty_sprite #clear the player's last position off the board.
+        Puzzle_board.map[win_element.y][win_element.x] = player_sprite #replace where the win sprite is with the player sprite in the printed array
+        break;
+
+    elif (move_message == "yes." or move_message == "push."):
+        player_indx = Objects_in_play.getIndexOfObj(player_sprite)
         #changes the player's coordinates, and the piece's coordinates (if needed)
         Objects_in_play.arr[player_indx].move(move_name, Puzzle_board, Objects_in_play);
 
@@ -75,36 +82,16 @@ while (time_counter < timeout):
         #so if the returned value was not "yes", then we'll just print out the error message that was returned instead.
         print(move_message);
 
-    new_map = objectsToMap(Objects_in_play.arr, Puzzle_board.map, empty_repr)
+    new_map = objectsToMap(Objects_in_play.arr, Puzzle_board.map, empty_sprite)
     #set the Puzzle_board.map to the updated map
     Puzzle_board.map = new_map
 
-    time_counter = time_counter + 1;
-    time.sleep(2)
+    moves_left = moves_left - 1;
+    if (moves_left <= 0):
+        continueGame = False
 
-    #getMove() asks for a move from the user, ni the form of a key press
-    move_name = getMove();
+    time.sleep(1)
 
-    player_index = Objects_in_play.getIndexOfObj(obj_id = player_avatar)
-    player_x = Objects_in_play.arr[player_index].x;
-    player_y = Objects_in_play.arr[player_index].y
 
-    move_message = Puzzle_board.validMove(player_x, player_y, move_name)
-
-    if (move_message == "yes." or move_message == "push."):
-        player_indx = Objects_in_play.getIndexOfObj(player_avatar)
-        #changes the player's coordinates, and the piece's coordinates (if needed)
-        Objects_in_play.arr[player_indx].move(move_name, Puzzle_board, Objects_in_play);
-
-    else:
-        #validMove() checks the objects, the player's new position after they move, and returns the string "yes" if it is a valid move, or returns a warning message to the user.
-        #so if the returned value was not "yes", then we'll just print out the error message that was returned instead.
-        print(move_message);
-
-    new_map = objectsToMap(Objects_in_play.arr, Puzzle_board.map, empty_repr, rock_repr)
-    #set the Puzzle_board.map to the updated map
-    Puzzle_board.map = new_map
-
-    #print the puzzle board each turn
-    print(Puzzle_board)
-    time += 1
+showBoard(Puzzle_board.map)
+print("End of game.")
